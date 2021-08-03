06b1511799bea8da10a8d7140fb091dd218761c7
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-03 17:55:24 -0300

    Exception handling - findById

--- src/main/java/com/gapp/course/services/UserService.java
+++ src/main/java/com/gapp/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gapp.course.entities.User;
 import com.gapp.course.repositories.UserRepository;
+import com.gapp.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
-		return user.get();
+		return user.orElseThrow((() -> new ResourceNotFoundException(id)));
 	}
 	
 	public User insert(User user) {
