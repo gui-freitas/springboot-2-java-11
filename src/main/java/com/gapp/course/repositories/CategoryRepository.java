5dbc41cc937df3e6da51f188eb9b071ed1b8228b
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-03 11:31:12 -0300

    Category

---
+++ src/main/java/com/gapp/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.gapp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gapp.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
