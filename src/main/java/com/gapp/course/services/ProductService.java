84a9eb0afc5323bef012dc21233ca5a62cbf3d48
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-03 11:41:09 -0300

    Product

---
+++ src/main/java/com/gapp/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.gapp.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gapp.course.entities.Product;
+import com.gapp.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll(){
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> product = productRepository.findById(id);
+		return product.get();
+	}
+}
