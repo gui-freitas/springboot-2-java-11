586a07e67edf0e3936b60c8989b047ad53d036ab
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-03 12:19:08 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/gapp/course/config/TestConfig.java
+++ src/main/java/com/gapp/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.gapp.course.entities.Category;
 import com.gapp.course.entities.Order;
+import com.gapp.course.entities.OrderItem;
 import com.gapp.course.entities.Product;
 import com.gapp.course.entities.User;
 import com.gapp.course.entities.enums.OrderStatus;
 import com.gapp.course.repositories.CategoryRepository;
+import com.gapp.course.repositories.OrderItemRepository;
 import com.gapp.course.repositories.OrderRepository;
 import com.gapp.course.repositories.ProductRepository;
 import com.gapp.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	public void run(String... args) throws Exception {
 		
@@ -68,5 +73,12 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	} 
 }
