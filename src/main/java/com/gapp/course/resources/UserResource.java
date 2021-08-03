ca9d5828e221c17924febf285ae8fd2ebe4a0ffa
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-03 09:23:23 -0300

    User entity and resource

---
+++ src/main/java/com/gapp/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.gapp.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gapp.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Lucas", "Lucas@email,com", "333333", "1234123");
+		return ResponseEntity.ok().body(u);
+	}
+}
