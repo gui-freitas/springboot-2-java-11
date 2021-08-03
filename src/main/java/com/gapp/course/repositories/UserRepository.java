b7ded31a4a1704a2a94d99f56dad1539d86007b1
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-03 09:43:40 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/gapp/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gapp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gapp.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
